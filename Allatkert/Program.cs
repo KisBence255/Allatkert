c7371f87f045eecd9007b3e560f71f3e28f56973
bence <bencekis255@gmail.com>
2026-03-30 22:25:02 +0200

    kesz

--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -64,12 +64,105 @@ Console.WriteLine($"reszlegek száma: {reszlegek.Count()}");
 
 //dolgozo legidosebb allata
 
-Console.Write("dolgzoo neve:");
-string dNev = Console.ReadLine();
+//Console.Write("dolgzoo neve:");
+//string dNev = Console.ReadLine();
 
-var idosallat = allatok
-    .Where(a => a.Gondozo.Nev.Contains(dNev))
-    .MaxBy(a => a.Eletkor);
+//var idosallat = allatok
+//    .Where(a => a.Gondozo.Nev.Contains(dNev))
+//    .MaxBy(a => a.Eletkor);
 
 
-Console.WriteLine($"Legidosebb allata: {idosallat}");
+//Console.WriteLine($"Legidosebb allata: {idosallat}");
+
+
+
+//-oroszlanok atlagos eletkora
+
+var oroszlanokkor = allatok.Where(a => a.Faj == "Oroszlán").Average(a=>a.Eletkor);
+Console.WriteLine("Oroszlánok átlagos életkora: "+oroszlanokkor);
+
+
+
+//-nevvel rendelkezo allatok szama
+var nevallatok = allatok.Where(a => a.Nev != null).Count();
+
+Console.WriteLine("Nevvel rendelkező állatok száma: "+nevallatok);
+
+//- 2 évesnél idosebb lakok a majom-szigeten
+var majomsziget = allatok.Where(a => a.Reszleg.Nev == "Majom-sziget" && a.Eletkor > 2).Count();
+
+Console.WriteLine("2 évesnél idősebb majom szigeten lakok:" + majomsziget);
+
+
+//- allatok szama gondozonkent
+
+var gondozonkent = allatok.GroupBy(a => a.Gondozo.Nev)
+    .Select(g => new
+    {
+        GondozoNev = g.Key,
+        AllatokSzama = g.Count()
+    });
+
+
+
+
+foreach (var item in gondozonkent)
+{
+    Console.WriteLine($" \n Gondozó: {item.GondozoNev}, Állatok száma: {item.AllatokSzama}");
+}
+
+
+
+//- nagy anna altal gondozott allatfajok szama
+
+var nagyanna = allatok.Where(a => a.Gondozo.Nev == "Nagy Anna").Select(a => a.Faj).Distinct().Count();
+
+Console.WriteLine("Nagy anna altal gonzott fajok szama:"+nagyanna);
+
+
+//- 2000 nm alatti alapteruletu reszlegen ello allatfajok
+
+var reszleg2000 = allatok.Where(a => a.Reszleg.Meret < 2000).Select(a => a.Faj).Distinct();
+
+Console.WriteLine("2000 nm alatti reszlegen elo allatfajok: ");
+foreach (var item in reszleg2000)
+{
+    Console.WriteLine("\t" + item);
+}
+
+//- reszlegek nepsurusege
+var nepsuruseg = allatok.GroupBy(a => a.Reszleg.Nev)
+    .Select(g => new
+    {
+        ReszlegNev = g.Key,
+        Nepsuruseg = g.Count() / (float)reszlegek.Single(r => r.Nev == g.Key).Meret
+    });
+foreach (var item in nepsuruseg)
+{
+    Console.WriteLine($"Részleg: {item.ReszlegNev}, Népsűrűség: {item.Nepsuruseg:0.00} állat/nm");
+}
+
+//- vizi allatokkal foglalkozo gondozo allatainak reszlegnevei
+
+Console.WriteLine("Vízi állatok gondozójának részlegei:");
+var vizigondozo = allatok
+   .Where(a => a.Gondozo.szakterulet == "Vízi állatok")
+   .Select(a => a.Reszleg.Nev)
+   .Distinct();
+
+foreach (var r in vizigondozo)
+    Console.WriteLine($"\t{r}");
+
+
+//- <input> allatfaj gondozojanak neve
+
+Console.Write("Állatfaj neve:");
+var fajnev = Console.ReadLine();
+
+var gondozoNeve = allatok
+    .Where(a => a.Faj.Contains(fajnev))
+    .Select(a => a.Gondozo.Nev)
+    .FirstOrDefault();
+Console.WriteLine(gondozoNeve);
+
+
